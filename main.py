1a498bef46f6cb8b9707ff61dc46d45203964860
Maxoso41 <sweetyzooble@gmail.com>
2023-11-23 11:24:34 +0100

    added logic (WIP)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 class Throw():
-    def __init__(self, npins1:int, npins2:int):
+    def __init__(self, npins1:int, npins2:int=0):
         self.npins1 = npins1
         self.npins2 = npins2
 
@@ -11,6 +11,48 @@ class Throw():
 throws:list[Throw] = []
 
 def calc_points():
-    points = 0
-    # TODO
-    return points
+    total_points = 0
+    frame = 1
+    throw_index = 0
+
+    while frame <= 10 and throw_index < len(throws):
+        current_throw = throws[throw_index]
+        total_points += current_throw.calc_points
+
+        if current_throw.npins1 == 10:  # Strike
+            total_points += calc_strike_bonus(throw_index)
+            throw_index += 1
+        elif current_throw.calc_points == 10:  # Spare
+            total_points += calc_spare_bonus(throw_index)
+            throw_index += 2
+        else:
+            throw_index += 2
+
+        frame += 1
+
+    return total_points
+
+def calc_strike_bonus(throw_index):
+    next_throw = throws[throw_index + 1]
+    return next_throw.calc_points
+
+def calc_spare_bonus(throw_index):
+    next_throw = throws[throw_index + 2]
+    return next_throw.npins1
+
+# Example usage:
+for ri in range(12):
+    pins = []
+    for ti in range(2):
+        pins.append(int(input(f"Runde {ri+1}, Wurf {ti+1}: Wie viele pins wurden umgeworfen?\n> ")))
+    throws.append(Throw(*pins))
+
+#throws.append(Throw(1, 4))
+#throws.append(Throw(4, 5))
+#throws.append(Throw(6, 4))  # Spare
+#throws.append(Throw(5, 5))  # Spare
+#throws.append(Throw(10))    # Strike
+#throws.append(Throw(0, 1))
+
+total_score = calc_points()
+print("Total Score:", total_score)
